2ce24f32f313a7e9dadfab0ba275c4e9aeabe8ad
maegop <megordon@alu.ucam.edu>
2022-02-11 09:33:16 +0100

    google analytics script added

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,49 @@ import { ChakraProvider } from "@chakra-ui/react";
 import SEO from "../next-seo.config";
 import { DefaultSeo } from "next-seo";
 import Head from "next/head";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider>
       <Head>
         <link rel="shortcut icon" href="/images/mem_happy.png" />
       </Head>
+
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       {/* It makes the same SEO config for each page */}
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
